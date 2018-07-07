1b75174485a335149df5cd8c17ffea6564c96972
wangtaoyuan <hdwangtaoyuan@126.com>
2018-07-07 10:56:45 +0800

    第一次提交，开机邮件提醒

---
+++ jsytx.py
@@ -0,0 +1,38 @@
+# encoding:utf-8
+'''
+Created on 2018年4月17日
+
+@author: wangtaoyuan
+'''  
+    
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+import panding
+
+
+line1 = '已开机'
+
+
+def mail():
+    ret = True
+    try:
+        msg=MIMEText(line1, "html", "utf-8")
+        msg['From']=formataddr(["开机提醒", panding.my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To']=formataddr([panding.user_name, panding.my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject']=panding.my_title  # 邮件的主题，也可以说是标题
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
+        server.login(panding.my_sender, panding.my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(panding.my_sender,[panding.my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 关闭连接
+    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        ret = False
+    return ret
+
+
+if __name__ == '__main__':
+    ret = mail()
+    if ret == False:
+        print('发送失败')
+        
+
---
+++ panding.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+'''
+Created on 2018年4月17日
+
+@author: wangtaoyuan
+'''
+
+
+my_sender = '846292076@qq.com'    # 发件人邮箱账号
+my_pass = 'szrjzhejehwfbeba'      # 发件人邮箱密码
+
+my_user='846292076@qq.com'      # 收件人邮箱账号，我这边发送给自己
+my_title='已开机'
+user_name='系统'
+
